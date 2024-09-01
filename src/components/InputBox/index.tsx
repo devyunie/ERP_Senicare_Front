59a58602a2b86296cd212ecfdf068c2f9f30a726
devyunie <myha001227@gmail.com>
2024-09-01 17:03:06 +0900

    senicare_front

---
+++ src/components/InputBox/index.tsx
@@ -0,0 +1,39 @@
+import React, { ChangeEvent, useState } from 'react'
+import './style.css'
+
+interface Props {
+    label: string;
+    type: 'text' | 'password'; //-> 리터럴 타입
+    placeholder: string;
+    value: string;
+    message: string;
+    messageError: boolean;
+    buttonName?: string;
+
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
+}
+
+export default function InputBox({
+    label,
+    type,
+    placeholder,
+    value,
+    buttonName,
+    message,
+    messageError,
+    onChange,
+    onButtonClick
+}: Props) {
+
+    return (
+        <div className="input-box">
+            <div className="label">{label}</div>
+            <div className="input-area">
+                <input value={value} type={type} placeholder={placeholder} onChange={onChange} />
+                {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick}>{buttonName}</div>}
+            </div>
+            <div className={`message ${messageError ? 'error' : 'primary'}`}>{message}</div>
+        </div>
+    )
+}

df5baa67a717f573c392506fe0a7afa07568ad88
devyunie <myha001227@gmail.com>
2024-09-17 18:58:57 +0900

    123

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Senicare from './Senicare';
 import { BrowserRouter } from 'react-router-dom';
+import "./ReactotronConfig"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
